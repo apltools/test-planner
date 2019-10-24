a658dbf9d07f5ffee7536a6e9cfbae36f259fd36
Marijn Jansen <marijn@jansen.onl>
2019-10-24 20:33:01 +0200

    certbot

--- init-certbot.sh
+++ init-certbot.sh
@@ -32,7 +32,8 @@ echo
 
 echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
-#mkdir -p "$data_path/conf/live/$domains"
+docker-compose run --rm --entrypoint"mkdir -p '$path'"
+
 docker-compose run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:1024 -days 1\
     -keyout '$path/privkey.pem' \
