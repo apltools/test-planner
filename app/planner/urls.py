7e77c58d375bd2035aee519983d4f4bf0efbfa32
Marijn Jansen <marijn@jansen.onl>
2019-10-27 19:18:35 +0100

    More messages for users on errors

--- app/planner/urls.py
+++ app/planner/urls.py
@@ -1,10 +1,12 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
     path('done/', views.done, name='done'),
     path('<slug:course_name>/', views.choose_date, name='choose_date'),
+    re_path(r'^(?P<course_name>[a-zA-Z0-9_-]+)/cancel/(?P<secret>[a-zA-Z0-9]{64})/?$', views.cancel_appointment,
+            name='cancel'),
     path('<slug:course_name>/<slug:date>/', views.choose_time, name='choose_time'),
     path('', views.index),
 ]
--- app/planner/views.py
+++ app/planner/views.py
@@ -1,13 +1,15 @@
-import datetime
+import datetime as dt
 
 from django.conf import settings
 from django.core.mail import send_mail
 from django.db import IntegrityError
-from django.http import HttpRequest, HttpResponse, Http404
+from django.http import Http404, HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.template.defaultfilters import date as _date, time as _time
+from django.urls import reverse
 
 from .forms import AppointmentForm
-from .models import Course, TestMoment, Appointment
+from .models import Appointment, Course, TestMoment
 
 
 def index(request) -> HttpResponse:
@@ -18,7 +20,7 @@ def choose_date(request: HttpRequest, course_name: str) -> HttpResponse:
     try:
         course = Course.objects.get(short_name=course_name)
     except Course.DoesNotExist:
-        raise Http404("Invalid course name")
+        raise Http404('Invalid course name')
 
     test_moments = course.tests_this_week()
     dates = test_moments.values_list('date', flat=True)
@@ -34,43 +36,62 @@ def choose_date(request: HttpRequest, course_name: str) -> HttpResponse:
 
 def choose_time(request: HttpRequest, course_name: str, date: str) -> HttpResponse:
     """View for choosing a time."""
+
+    # Validate course
     try:
         course = Course.objects.get(short_name=course_name)
     except Course.DoesNotExist:
-        raise Http404("Invalid Course")
+        raise Http404('Invalid Course')
 
+    # Validate date
     try:
-        date_obj = datetime.date.fromisoformat(date)
+        date_obj = dt.date.fromisoformat(date)
         test_moment = TestMoment.objects.get(date=date_obj)
     except (TestMoment.DoesNotExist, ValueError):
-        raise Http404("Invalid date")
+        raise Http404('Invalid date')
+
+    # Validate of date isn't in the future.
+    if test_moment.date <= dt.date.today():
+        return render(request, 'planner/error.html',
+                      {'error_message': 'Deze datum is inmiddels verlopen.',
+                       'course': course, })
 
     if request.method == "POST":
         form = AppointmentForm(request.POST)
 
         if form.is_valid():
+            if not test_moment.time_available(form.cleaned_data['start_time'], course):
+                return render(request, 'planner/error.html',
+                              {'error_message': 'Waarschijnlijk was iemand je voor, dit tijdstip is helaas al vol.',
+                               'course': course, })
+
             app = Appointment()
             app.student_name = form.cleaned_data['student_name']
             app.student_nr = form.cleaned_data['student_nr']
             app.email = form.cleaned_data['email']
             app.date = date_obj
             app.course = course
-            app.start_time = form.data["start_time"]
+            app.start_time = form.cleaned_data["start_time"]
             app.duration = test_moment.test_length
 
+            # Save can go wrong is student had a appoitment on this date
             try:
                 app.save()
             except IntegrityError:
-                raise Http404("Duplicate appointment on date is not allowed")
+                return render(request, 'planner/error.html',
+                              {'error_message': 'Het maken van een dubbele afspraak op een dag is niet toegestaan.',
+                               'course': course, })
 
             app.tests.set(form.cleaned_data['tests'])
-            send_confirm_email(course=course, appointment=app, test_moment=test_moment)
+
+            send_confirm_email(course=course, appointment=app, test_moment=test_moment, request=request)
+
             return done(request, course=course, app=app, tm=test_moment)
 
     else:
         form = AppointmentForm()
 
-    # Insert the allowed tests for the course into the form.
+    # Insert the allowed tests for the course into the form as options.
     form.fields['tests'].queryset = test_moment.coursemoment_set.get(course__exact=course).allowed_tests.all()
 
     context = {
@@ -92,10 +113,31 @@ def done(request: HttpRequest, *, course: Course, app: Appointment, tm: TestMome
     return render(request, 'planner/done.html', context=context)
 
 
-def send_confirm_email(*, course: Course, appointment: Appointment, test_moment: TestMoment):
-    if settings.EMAIL_HOST:
+def send_confirm_email(*, course: Course, appointment: Appointment, test_moment: TestMoment, request: HttpRequest):
+    url = request.build_absolute_uri(
+        reverse("cancel", kwargs={"course_name": course.short_name, "secret": appointment.cancel_secret}))
+
+    message = f'Je hebt je ingeschreven voor het maken van een toetsje op {_date(appointment.date, "l j F")} ' \
+              f'om {_time(appointment.start_time)}.\r\nHet maken van dit toetsje vind plaats in {test_moment.location}.\r\n' \
+              f'Wil de de afspraak anuleren, dat kan via deze link {url}'
+
+    if not settings.EMAIL_HOST:
+        print(message)
         return
-    send_mail(subject=f"Toetsje ingepland voor {course.name}",
-              message=f"Op {appointment.date} om {appointment.start_time} heb je een toetsje ingepland.\r\nHet maken van dit toetsje vind plaats in {test_moment.location}.",
+    send_mail(subject=f'Toetsje ingepland voor {course.name}',
+              message=message,
               from_email=settings.EMAIL_FROM,
               recipient_list=[appointment.email])
+
+
+def cancel_appointment(request: HttpRequest, course_name: str, secret: str):
+    try:
+        course = Course.objects.get(short_name__exact=course_name)
+    except Course.DoesNotExist:
+        raise Http404("Invalid Course")
+    try:
+        Appointment.objects.get(cancel_secret__exact=secret, course__exact=course).delete()
+    except Appointment.DoesNotExist:
+        return render(request, 'planner/error.html',
+                      {'error_message': 'Ongeldige link',
+                       'course': course})
