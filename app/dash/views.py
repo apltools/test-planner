9997a3b2dd0c39f63173616fab8c5447b3973d80
Marijn Jansen <marijn@jansen.onl>
2019-11-11 16:07:59 +0100

    History and Home split.

--- app/dash/views.py
+++ app/dash/views.py
@@ -1,17 +1,33 @@
-from typing import List, Tuple
+from typing import List
 
+import django.utils.timezone as tz
 from django.contrib.admin.views.decorators import staff_member_required
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-from planner.models import TestMoment, TimeAppointmentsTuple
+from planner.models import TestMoment
+
+nav_items = {
+    'Home': 'index',
+    'History': 'history',
+    'Admin': 'admin:index',
+}
 
 
 @staff_member_required
 def index(request: HttpRequest) -> HttpResponse:
-    test_moments: List[TestMoment] = TestMoment.objects.all().order_by('date', 'start_time')
+    test_moments: List[TestMoment] = TestMoment.objects.filter(date__gte=tz.localdate()).order_by('date', 'start_time')
 
-    context = {'test_moments': test_moments}
+    context = {'test_moments': test_moments,
+               'nav_items': nav_items}
 
     return render(request, 'dash/index.html', context=context)
 
+
+@staff_member_required
+def history(request: HttpRequest) -> HttpResponse:
+    test_moments: List[TestMoment] = TestMoment.objects.filter(date__lt=tz.localdate()).order_by('date', 'start_time')
+    context = {'test_moments': test_moments,
+               'nav_items': nav_items}
+
+    return render(request, 'dash/index.html', context=context)
